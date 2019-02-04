3d6dda895f18ee6ac6d31afb95eec19cc1dfdb43
Matthieu <matthieunadeau96@gmail.com>
2019-02-04 13:21:02 -0500

    Condensed the components into one called 'Key'

--- App.js
+++ App.js
@@ -5,39 +5,37 @@ import {
   StyleSheet,
 } from 'react-native'
 
-import DigitKey from './src/components/DigitKey'
-import FunctionKey from './src/components/FunctionKey'
-import OperationKey from './src/components/OperationKey'
+import Key from './src/components/Key'
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.functionKeys}>
-          <FunctionKey title={'AC'} />
-          <FunctionKey title={'+-'} />
-          <FunctionKey title={'%'} />
+          <Key title={'AC'} />
+          <Key title={'+-'} />
+          <Key title={'%'} />
         </View>
         <View style={styles.digitKeys}>
-          <DigitKey title={'0'} />
-          <DigitKey title={'.'} />
-          <DigitKey title={'1'} />
-          <DigitKey title={'2'} />
-          <DigitKey title={'3'} />
-          <DigitKey title={'4'} />
-          <DigitKey title={'5'} />
-          <DigitKey title={'6'} />
-          <DigitKey title={'7'} />
-          <DigitKey title={'8'} />
-          <DigitKey title={'9'} />
+          <Key title={'0'} />
+          <Key title={'.'} />
+          <Key title={'1'} />
+          <Key title={'2'} />
+          <Key title={'3'} />
+          <Key title={'4'} />
+          <Key title={'5'} />
+          <Key title={'6'} />
+          <Key title={'7'} />
+          <Key title={'8'} />
+          <Key title={'9'} />
         </View>
         <View style={styles.operationKeys}>
-          <OperationKey title={'/'} />
-          <OperationKey title={'*'} />
-          <OperationKey title={'x'} />
-          <OperationKey title={'-'} />
-          <OperationKey title={'+'} />
-          <OperationKey title={'='} />
+          <Key title={'/'} />
+          <Key title={'*'} />
+          <Key title={'x'} />
+          <Key title={'-'} />
+          <Key title={'+'} />
+          <Key title={'='} />
         </View>
       </View>
     )
@@ -51,12 +49,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   functionKeys: {
-
+    flexDirection: 'row',
   },
   digitKeys: {
-
+    flexDirection: 'row',
   },
   operationKeys: {
-
+    flexDirection: 'row',
   },
 })

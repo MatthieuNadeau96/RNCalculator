da3f55254230c61c8fb0f0b50ce62035a11e8ca8
Matthieu <matthieunadeau96@gmail.com>
2019-02-07 13:10:54 -0500

    added the buttons to their proper spacing and added calculationDisplay and resultDisplay

--- App.js
+++ App.js
@@ -18,21 +18,34 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
 
-        <View style={styles.resultDisplay}></View>
-        <View style={styles.calculatonDisplay}></View>
+        <View style={styles.calculatonDisplay}>
+          <Text style={styles.calculationText}>11*11</Text>
+        </View>
+        <View style={styles.resultDisplay}>
+          <Text style={styles.resultText}>121</Text>
+        </View>
         <View style={styles.keyPad}>
           <View style={styles.digitKeys}>
-            <Key title={'0'} />
-            <Key title={'.'} />
-            <Key title={'1'} />
-            <Key title={'2'} />
-            <Key title={'3'} />
-            <Key title={'4'} />
-            <Key title={'5'} />
-            <Key title={'6'} />
-            <Key title={'7'} />
-            <Key title={'8'} />
-            <Key title={'9'} />
+            <View style={styles.btnRow}>
+              <Key title={'0'} />
+              <Key title={'.'} />
+              <Key title={'1'} />
+            </View>
+            <View style={styles.btnRow}>
+              <Key title={'2'} />
+              <Key title={'3'} />
+              <Key title={'4'} />
+            </View>
+            <View style={styles.btnRow}>
+              <Key title={'5'} />
+              <Key title={'6'} />
+              <Key title={'7'} />
+            </View>
+            <View style={styles.btnRow}>
+              <Key title={'8'} />
+              <Key title={'9'} />
+              <Key title={'9'} />
+            </View>
           </View>
           <View style={styles.operationKeys}>
             <Key title={'DEL'} />
@@ -53,14 +66,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  resultDisplay: {
-    flex: 2,
-  },
   calculatonDisplay: {
+    flex: 2,
     alignItems: 'flex-end',
-    justifyContent: 'flex-end',
+    justifyContent: 'center',
+  },
+  resultDisplay: {
+    alignItems: 'flex-end',
+    justifyContent: 'center',
     backgroundColor: '#b5cbff',
     flex: 1,
+  },
+  calculationText: {
+    fontSize: 30,
+    color: '#000000',
+  },
+  resultText: {
+    fontSize: 24,
+    color: '#ffffff',
   },
   keyPad: {
     backgroundColor: '#9ee0a5',
@@ -69,14 +92,19 @@ const styles = StyleSheet.create({
     padding: 5,
     flex: 7,
   },
+  btnRow: {
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
   digitKeys: {
     flex: 3,
-    flexDirection: 'row',
     backgroundColor: '#ed8c8c',
   },
   operationKeys: {
     flex: 1,
+    justifyContent: 'space-around',
     backgroundColor: '#8e74db',
-    flexDirection: 'row',
   },
 })
